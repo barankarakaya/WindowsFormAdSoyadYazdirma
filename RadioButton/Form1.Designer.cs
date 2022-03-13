2827c2f9d7aed1f556d39feab881b7e7c06aba53
baran <bkarakaya0707@gmail.com>
2022-03-13 14:42:18 +0300

    Add project files.

---
+++ RadioButton/Form1.Designer.cs
@@ -0,0 +1,136 @@
+﻿
+namespace RadioButton
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.txt_ad = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txt_soyad = new System.Windows.Forms.TextBox();
+            this.radio_erkek = new System.Windows.Forms.RadioButton();
+            this.radio_kadin = new System.Windows.Forms.RadioButton();
+            this.btn_yazdir = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(41, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Adınız :";
+            // 
+            // txt_ad
+            // 
+            this.txt_ad.Location = new System.Drawing.Point(69, 6);
+            this.txt_ad.Name = "txt_ad";
+            this.txt_ad.Size = new System.Drawing.Size(149, 20);
+            this.txt_ad.TabIndex = 0;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 42);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(58, 13);
+            this.label2.TabIndex = 0;
+            this.label2.Text = "Soyadınız :";
+            // 
+            // txt_soyad
+            // 
+            this.txt_soyad.Location = new System.Drawing.Point(69, 39);
+            this.txt_soyad.Name = "txt_soyad";
+            this.txt_soyad.Size = new System.Drawing.Size(149, 20);
+            this.txt_soyad.TabIndex = 1;
+            // 
+            // radio_erkek
+            // 
+            this.radio_erkek.AutoSize = true;
+            this.radio_erkek.Location = new System.Drawing.Point(15, 72);
+            this.radio_erkek.Name = "radio_erkek";
+            this.radio_erkek.Size = new System.Drawing.Size(53, 17);
+            this.radio_erkek.TabIndex = 2;
+            this.radio_erkek.TabStop = true;
+            this.radio_erkek.Text = "Erkek";
+            this.radio_erkek.UseVisualStyleBackColor = true;
+            // 
+            // radio_kadin
+            // 
+            this.radio_kadin.AutoSize = true;
+            this.radio_kadin.Location = new System.Drawing.Point(112, 72);
+            this.radio_kadin.Name = "radio_kadin";
+            this.radio_kadin.Size = new System.Drawing.Size(52, 17);
+            this.radio_kadin.TabIndex = 3;
+            this.radio_kadin.TabStop = true;
+            this.radio_kadin.Text = "Kadın";
+            this.radio_kadin.UseVisualStyleBackColor = true;
+            // 
+            // btn_yazdir
+            // 
+            this.btn_yazdir.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.btn_yazdir.Location = new System.Drawing.Point(12, 106);
+            this.btn_yazdir.Name = "btn_yazdir";
+            this.btn_yazdir.Size = new System.Drawing.Size(206, 23);
+            this.btn_yazdir.TabIndex = 4;
+            this.btn_yazdir.Text = "Yazdır";
+            this.btn_yazdir.UseVisualStyleBackColor = true;
+            this.btn_yazdir.Click += new System.EventHandler(this.btn_yazdir_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(230, 142);
+            this.Controls.Add(this.btn_yazdir);
+            this.Controls.Add(this.radio_kadin);
+            this.Controls.Add(this.radio_erkek);
+            this.Controls.Add(this.txt_soyad);
+            this.Controls.Add(this.txt_ad);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Ad Soyad CinsiyeT";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txt_ad;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txt_soyad;
+        private System.Windows.Forms.RadioButton radio_erkek;
+        private System.Windows.Forms.RadioButton radio_kadin;
+        private System.Windows.Forms.Button btn_yazdir;
+    }
+}
+
